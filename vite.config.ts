7c97ced8a197db5f977d722fa1d1533d19b0839e
SHIVAM191427 <130179714+SHIVAM191427@users.noreply.github.com>
2025-09-16 21:56:40 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-main/", // <-- replace with your repo name
+  base: "/Portfolio-main/", // must match your repo name exactly
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
